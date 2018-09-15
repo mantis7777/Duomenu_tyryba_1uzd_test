a665a176531877eefbf0bc5075b760da15cf098a
Mantas Kildišis <s029875@ad.viko.lt>
2018-09-15 17:50:07 +0300

    Pridejau mygtuka

--- Duomenu_tyryba_1uzd_test/Form1.Designer.cs
+++ Duomenu_tyryba_1uzd_test/Form1.Designer.cs
@@ -30,33 +30,48 @@
         {
             this.vAll = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // vAll
             // 
             this.vAll.AutoSize = true;
-            this.vAll.Location = new System.Drawing.Point(73, 175);
+            this.vAll.Location = new System.Drawing.Point(55, 142);
+            this.vAll.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.vAll.Name = "vAll";
-            this.vAll.Size = new System.Drawing.Size(0, 17);
+            this.vAll.Size = new System.Drawing.Size(0, 13);
             this.vAll.TabIndex = 0;
             // 
             // groupBox1
             // 
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(9, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1039, 137);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(779, 111);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Viešbučiai mieste";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(145, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1077, 514);
+            this.ClientSize = new System.Drawing.Size(808, 418);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.vAll);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -69,6 +84,7 @@
 
         private System.Windows.Forms.Label vAll;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
